018c874fe5c138bb4d102115b21f97b64c7d5187
Sangram2112 <91899500+Sangram2112@users.noreply.github.com>
2022-06-30 12:43:37 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Fifa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPlayerName = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Email = new System.Windows.Forms.Label();
+            this.txtPlayerClub = new System.Windows.Forms.TextBox();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtJersyNo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btCsv = new System.Windows.Forms.Button();
+            this.btExcel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(40, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player Name";
+            // 
+            // txtPlayerName
+            // 
+            this.txtPlayerName.Location = new System.Drawing.Point(172, 89);
+            this.txtPlayerName.Name = "txtPlayerName";
+            this.txtPlayerName.Size = new System.Drawing.Size(125, 20);
+            this.txtPlayerName.TabIndex = 1;
+            this.txtPlayerName.TextChanged += new System.EventHandler(this.txtUserName_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ImeMode = System.Windows.Forms.ImeMode.Alpha;
+            this.dataGridView1.Location = new System.Drawing.Point(148, 278);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(531, 150);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.BackColor = System.Drawing.Color.Black;
+            this.Email.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Email.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Email.Location = new System.Drawing.Point(67, 140);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(40, 18);
+            this.Email.TabIndex = 3;
+            this.Email.Text = "Club";
+            // 
+            // txtPlayerClub
+            // 
+            this.txtPlayerClub.Location = new System.Drawing.Point(172, 138);
+            this.txtPlayerClub.Name = "txtPlayerClub";
+            this.txtPlayerClub.Size = new System.Drawing.Size(125, 20);
+            this.txtPlayerClub.TabIndex = 4;
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(172, 182);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(125, 20);
+            this.txtCountry.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(56, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Country";
+            // 
+            // txtJersyNo
+            // 
+            this.txtJersyNo.Location = new System.Drawing.Point(172, 229);
+            this.txtJersyNo.Name = "txtJersyNo";
+            this.txtJersyNo.Size = new System.Drawing.Size(125, 20);
+            this.txtJersyNo.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(53, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Jersy No";
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnSave.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnSave.FlatAppearance.BorderSize = 2;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(351, 89);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(99, 33);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(351, 156);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(99, 33);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(350, 222);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(99, 32);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Fifa.Properties.Resources.ec82a51e2d701406118f5d010b7fa0ab;
+            this.pictureBox1.Location = new System.Drawing.Point(208, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(389, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btCsv
+            // 
+            this.btCsv.BackgroundImage = global::Fifa.Properties.Resources.icons8_export_csv_96;
+            this.btCsv.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btCsv.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCsv.Image = ((System.Drawing.Image)(resources.GetObject("btCsv.Image")));
+            this.btCsv.Location = new System.Drawing.Point(517, 185);
+            this.btCsv.Name = "btCsv";
+            this.btCsv.Size = new System.Drawing.Size(115, 41);
+            this.btCsv.TabIndex = 14;
+            this.btCsv.Text = "Export To CSV";
+            this.btCsv.UseVisualStyleBackColor = true;
+            this.btCsv.Click += new System.EventHandler(this.btCsv_Click);
+            // 
+            // btExcel
+            // 
+            this.btExcel.BackgroundImage = global::Fifa.Properties.Resources.icons8_microsoft_excel_96;
+            this.btExcel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btExcel.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btExcel.FlatAppearance.BorderSize = 4;
+            this.btExcel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btExcel.Image = global::Fifa.Properties.Resources.icons8_microsoft_excel_2019_96;
+            this.btExcel.Location = new System.Drawing.Point(517, 114);
+            this.btExcel.Name = "btExcel";
+            this.btExcel.Size = new System.Drawing.Size(115, 44);
+            this.btExcel.TabIndex = 12;
+            this.btExcel.Text = "Export To Excel";
+            this.btExcel.UseVisualStyleBackColor = true;
+            this.btExcel.Click += new System.EventHandler(this.btExcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btCsv);
+            this.Controls.Add(this.btExcel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtJersyNo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPlayerClub);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtPlayerName);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPlayerName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.TextBox txtPlayerClub;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtJersyNo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btExcel;
+        private System.Windows.Forms.Button btCsv;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+
+namespace Fifa
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        string connString = @"Data Source=DESKTOP-UAK5LC3\SQLEXPRESS;Initial Catalog=Fifa;Integrated Security=True";
+        //conn = new SqlConnection(connString);
+        SqlCommand cmd;
+        SqlDataAdapter adpt;
+        DataTable dt;
+        int Player_Id;
+
+        public Form1()
+        {
+            InitializeComponent();
+            displayInfo();
+        }
+
+        public static void ExportExcel(string fileName, DataGridView dataGridView1)
+        {
+            if (dataGridView1.Rows.Count > 0)
+            {
+                string saveFileName = "";
+                SaveFileDialog saveDialog = new SaveFileDialog();
+                saveDialog.DefaultExt = "xls";
+                saveDialog.Filter = "Excel File|*.xls";
+                saveDialog.FileName = fileName;
+                saveDialog.ShowDialog();
+                saveFileName = saveDialog.FileName;
+                if (saveFileName.IndexOf(":") < 0) return;
+                Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+
+                Microsoft.Office.Interop.Excel.Workbooks workbooks = xlApp.Workbooks;
+                Microsoft.Office.Interop.Excel.Workbook workbook = workbooks.Add(Microsoft.Office.Interop.Excel.XlWBATemplate.xlWBATWorksheet);
+                Microsoft.Office.Interop.Excel.Worksheet worksheet = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Worksheets[1];
+
+                //header
+                for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                {
+                    worksheet.Cells[1, i + 1] = dataGridView1.Columns[i].HeaderText;
+                }
+                //storing each row and column value for excel sheet
+                for (int r = 0; r < dataGridView1.Rows.Count; r++)
+                {
+                    for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                    {
+                        worksheet.Cells[r + 2, i + 1] = dataGridView1.Rows[r].Cells[i].Value;
+                    }
+                    Application.DoEvents();
+                }
+                worksheet.Columns.EntireColumn.AutoFit();
+
+                if (saveFileName != "")
+                {
+                    try
+                    {
+                        workbook.Saved = true;
+                        workbook.SaveCopyAs(saveFileName);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("The file may be opened now, Data not export\n" + ex.Message);
+                    }
+
+                }
+                xlApp.Quit();
+                MessageBox.Show("File save successful", "prompt", MessageBoxButtons.OK);
+            }
+            else
+            {
+                MessageBox.Show("the datagridview is empty", "prompt", MessageBoxButtons.OK);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //SqlConnection conn;
+            //string connString = @"Data Source=DESKTOP-UAK5LC3\SQLEXPRESS;Initial Catalog=Fifa;Integrated Security=True";
+            conn = new SqlConnection(connString);
+
+            conn.Open();
+            cmd = new SqlCommand("insert into player1 values('" + txtPlayerName.Text + "','" + txtPlayerClub.Text + "','" + txtCountry.Text + "','" + txtJersyNo.Text + "')", conn);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Player Data Stored in the DataBase");
+            //displayInfo();
+            conn.Close();
+            displayInfo();
+            Clear();
+
+            /*
+            System.Data.OleDb.OleDbConnection MyConnection;
+            System.Data.OleDb.OleDbCommand myCommand = new System.Data.OleDb.OleDbCommand();
+            string sql = null;
+            MyConnection = new System.Data.OleDb.OleDbConnection("provider=Microsoft.Jet.OLEDB.4.0;Data Source='C:\\Users\\sangr\\OneDrive\\Desktop\\Fifa\\Excel\\Fifa.xls';Extended Properties=Excel 8.0;");
+            MyConnection.Open();
+            myCommand.Connection = MyConnection;
+            sql = "Insert into [Sheet1$] (id,name)  values('5','e')";
+            myCommand.CommandText = sql;
+            myCommand.ExecuteNonQuery();
+            MyConnection.Close();*/
+        }
+
+        public void displayInfo()
+        {
+            conn = new SqlConnection(connString);
+            conn.Open();
+            adpt = new SqlDataAdapter("select * from player1", conn);
+            dt = new DataTable();
+            adpt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+
+        }
+
+        public void Clear()
+        {
+            txtPlayerName.Text = "";
+            txtPlayerClub.Text = "";
+            txtCountry.Text = "";
+            txtJersyNo.Text = "";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Player_Id = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            txtPlayerName.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtPlayerClub.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtCountry.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txtJersyNo.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            conn = new SqlConnection(connString);
+            conn.Open();
+            cmd = new SqlCommand("update player1 set P_Name ='" + txtPlayerName.Text + "',Club='" + txtPlayerClub.Text + "',Country='" + txtCountry.Text + "',J_No='" + txtJersyNo.Text + "' where Player_Id='" + Player_Id + "'", conn);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Player Information Has Been Updated !");
+            //displayInfo();
+            Clear();
+            conn.Close();
+            displayInfo();
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(connString);
+            conn.Open();
+            cmd = new SqlCommand("delete from player1 where Player_Id = '" + Player_Id + "'", conn);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Player Data Deleted");
+            conn.Close();
+            displayInfo();
+        }
+
+        private void txtUserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btExcel_Click(object sender, EventArgs e)
+        {
+            string str = "Data";
+            ExportExcel(str, dataGridView1);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            string path = @"C:\Users\sangr\OneDrive\Desktop\Fifa\Excel\Fifa.xls";
+            xlWorkBook.SaveAs(path, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+            MessageBox.Show("Excel file created");*/
+        }
+
+        private void btPdf_Click(object sender, EventArgs e)
+        {
+            /*PdfPTable table = new PdfPTable(dataGridView1.Columns.Count);
+
+            for (int j = 0; j < dataGridView1.Columns.Count; j++)
+            {
+                table.AddCell(new Phrase(dataGridView1.Columns[j].HeaderText));
+            }
+
+            table.HeaderRows = 1;
+
+            for(int i = 0 ; i < dataGridView1.Rows.Count; i++)
+            {
+                for(int k = 0; k < dataGridView1.Columns.Count; k++)
+                {
+                    if(dataGridView1[k,i].Value!=null)
+                    {
+                        table.AddCell(new Phrase(dataGridView1[k, i].Value.ToString()));
+                    }
+                }
+            }*/
+        }
+
+        private void btCsv_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.InitialDirectory = "C";
+            dlg.Title = "SaveAs";
+            dlg.FileName = "";
+            dlg.Filter = "CSV (Comma delimited)|*.csv";
+            if (dlg.ShowDialog() != DialogResult.Cancel)
+            {
+                Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
+                ExcelApp.Application.Workbooks.Add(Type.Missing); //creating workbook
+
+                ExcelApp.Columns.ColumnWidth = 20;
+                //header
+                for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+                {
+                    ExcelApp.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+                }
+                //storing each row and column value for excel sheet
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        if (dataGridView1.Rows[i].Cells[j].Value != null)
+                        {
+                            ExcelApp.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                        }
+
+                    }
+                }
+                if (dlg.FileName != "")
+                {
+                    try
+                    {
+                        ExcelApp.ActiveWorkbook.SaveCopyAs(dlg.FileName.ToString());
+                        ExcelApp.ActiveWorkbook.Saved = true;
+                        ExcelApp.Quit();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("export error, the file may be opened now\n" + ex.Message);
+                    }
+
+                }
+                MessageBox.Show("File save successful", "prompt", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
+
+
